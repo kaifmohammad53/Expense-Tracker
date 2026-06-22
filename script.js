096db75a1a4d189b0d599976b1dd92c45e576a7e
kaifmohammad53 <kaifmohammad9530@gmail.com>
2026-06-22 10:34:24 +0530

    js modification

--- script.js
+++ script.js
@@ -1,2 +1,4 @@
 let amtEnter=document.querySelector("#amtEnter");
 let desEnter=document.querySelector("#desEnter");
+let inputbtn=document.querySelector("#income");
+let expensbtn=document.querySelector("#expense");

5e2524ee9b95fddd124bd9e7fa9dea2459ddb8f4
kaifmohammad53 <kaifmohammad9530@gmail.com>
2026-06-23 11:15:28 +0530

    accessing element done

--- script.js
+++ script.js
@@ -2,3 +2,7 @@ let amtEnter=document.querySelector("#amtEnter");
 let desEnter=document.querySelector("#desEnter");
 let inputbtn=document.querySelector("#income");
 let expensbtn=document.querySelector("#expense");
+let total =document.querySelector("#total");
+let addIncomre=document.querySelector("#addIncome");
+let subExpense=document.querySelector("#subExpense");
+let trsHistory= document.querySelector(".trsHistory");
